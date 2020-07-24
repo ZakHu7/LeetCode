7d726085552519656f1781d562c17b1fe705098f
Zak Hu <zakhu7@hotmail.com>
2020-07-23 22:35:46 -0400

    Q35 Binary Search

---
+++ 1-99/Q35.py
@@ -0,0 +1,29 @@
+#Search for where element is, or where it would be if it was in the array
+
+#binary search
+
+class Solution:
+    def searchInsert(self, nums: [int], target: int) -> int:
+        l, r, m = 0, len(nums)-1, 0
+
+        if not nums: return 0
+
+        while l < r:
+          m = l + (r-l)//2
+
+          if nums[m] == target:
+            return m
+          elif target > nums[m]:
+            l = m + 1
+          else:
+            r = m
+        if nums[l] == target or target < nums[l]:
+          return l
+
+        return l + 1
+
+s = Solution()
+nums = [1,3,5,6]
+target = 2
+
+print(s.searchInsert(nums, target))
