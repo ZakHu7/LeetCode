69d14bd31d0b764b98f9d64ea705497ff53ba311
Zak Hu <zakhu7@hotmail.com>
2020-07-23 22:40:23 -0400

    Q35 Optimal l <= r instead

--- 1-99/Q35.py
+++ 1-99/Q35.py
@@ -22,6 +22,22 @@ class Solution:
 
         return l + 1
 
+    ## better solution with less duplication
+    def searchInsert2(self, nums: [int], target: int) -> int:
+        l, r, m = 0, len(nums)-1, 0
+
+        while l <= r:
+          m = l + (r-l)//2
+
+          if nums[m] == target:
+            return m
+          elif target > nums[m]:
+            l = m + 1
+          else:
+            r = m - 1
+
+        return l
+
 s = Solution()
 nums = [1,3,5,6]
 target = 2
