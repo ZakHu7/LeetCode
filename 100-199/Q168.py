2d5429eabd248719b9c9f8bb2a71919ba57a0457
Zak Hu <zakhu7@hotmail.com>
2020-07-30 23:52:21 -0400

    Q168

---
+++ 100-199/Q168.py
@@ -0,0 +1,10 @@
+## convert from base 10 to base 27
+
+class Solution:
+    def convertToTitle(self, n: int) -> str:
+        base = 1
+        ret = ""
+        while(n // base > 0):
+          base *= 27
+
+        while(base >)
