64960924e20d439b2d29f7f8dec65dafe3c1695d
Zak Hu <zakhu7@hotmail.com>
2020-07-31 00:49:01 -0400

    Q168 finished

--- 100-199/Q168.py
+++ 100-199/Q168.py
@@ -1,10 +1,16 @@
-## convert from base 10 to base 27
+## count every 26^i
 
 class Solution:
     def convertToTitle(self, n: int) -> str:
-        base = 1
         ret = ""
-        while(n // base > 0):
-          base *= 27
 
-        while(base >)
+        while(n > 0):
+          ret = chr(ord('A') + (n-1)%26) + ret
+          n = (n-1) // 26
+        
+        return ret
+
+
+s = Solution()
+n = 53
+print(s.convertToTitle(n))
